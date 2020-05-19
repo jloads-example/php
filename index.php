ddbc12839e88165404b8564961b2093929add8e9
tom <tom@sapletta.com>
2020-05-19 22:10:17 +0200

    response

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 var_dump($_SERVER['HTTP_ORIGIN']);
 var_dump($_SERVER);
-die;
+return 0;
     $http_origin = $_SERVER['HTTP_ORIGIN'];
     $allowed_origins = [
         "https://app.jloads.com",
