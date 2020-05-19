6d66a9e6dd71af5e1bab5f43e5f28cc1636a4bb7
tom <tom@sapletta.com>
2020-05-19 20:27:51 +0200

    cors

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
     header('Access-Control-Allow-Origin: *');
     header("Access-Control-Allow-Credentials: true");
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1000');
+    header("Access-Control-Expose-Headers: Content-Length, X-JSON");
+    header('Access-Control-Max-Age: 86400');
     header('Access-Control-Allow-Headers: Origin, Content-Type, X-Requested-With, X-Auth-Token , Authorization');
     header('Content-Type: application/json');
 
