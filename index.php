f0470b955e0fb20609fb2ab086ac810abef921fc
tom <tom@sapletta.com>
2020-05-19 20:17:06 +0200

    cors

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     header("Access-Control-Allow-Credentials: true");
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Requested-With, X-Auth-Token , Authorization');
     header('Content-Type: application/json');
 
 /*
